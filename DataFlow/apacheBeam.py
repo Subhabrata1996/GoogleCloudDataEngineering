06605c221e7e1549f55bd337242d766ee47ca3ad
Subhabrata1996 <subhabrataiam@gmail.com>
2021-02-19 00:15:57 +0530

    changed ParDo to Map for JSONtoCSV

--- DataFlow/apacheBeam.py
+++ DataFlow/apacheBeam.py
@@ -12,7 +12,7 @@ import pandas as pd
 from apache_beam.io import WriteToText #Streaming pipeline not supported yet for python sdk
 from apache_beam.io import fileio
 
-def roundTime(dt=None, roundTo=60):
+def roundTime(dt=None, roundTo=1):
    if dt == None : dt = datetime.datetime.now()
    seconds = (dt.replace(tzinfo=None) - dt.min).seconds
    rounding = (seconds+roundTo/2) // roundTo * roundTo
@@ -28,10 +28,9 @@ class interpolateSensors(beam.DoFn):
     json_string["timestamp"] = timestamp
     return [json_string]
 
-class convertToCsv(beam.DoFn):
-  def process(self,jsonData):     
-      csvStr =  ",".join(str(elements) for elements in list(jsonData.values()))
-      return csvStr
+def convertToCsv(jsonData):    
+    csvStr =  ",".join(str(elements) for elements in list(jsonData.values()))
+    return csvStr
 
 def run(subscription_name, output_table, output_gcs_path, interval=1.0, pipeline_args=None):
     schema = 'Timestamp:TIMESTAMP, PRESSURE_1:FLOAT, PRESSURE_2:FLOAT, PRESSURE_3:FLOAT, PRESSURE_4:FLOAT, PRESSURE_5:FLOAT'
@@ -40,7 +39,7 @@ def run(subscription_name, output_table, output_gcs_path, interval=1.0, pipeline
         | 'ReadData' >> beam.io.ReadFromPubSub(subscription=subscription_name)
         | "Decode" >> beam.Map(lambda x: x.decode('utf-8'))
         | "Convert to list" >> beam.Map(lambda x: x.split(","))
-        | "to tuple" >> beam.Map(lambda x: (roundTime(datetime.datetime.strptime(x[0],'%Y-%m-%d %H:%M:%S.%f'), roundTo = interval),[x[1] , float(x[2])]))#{x.split(',')[1]:x.split(',')[2]}))
+        | "to tuple" >> beam.Map(lambda x: (roundTime(datetime.datetime.strptime(x[0],'%Y-%m-%d %H:%M:%S.%f'), roundTo = interval),[x[1] , float(x[2])]))
         | "Window" >> beam.WindowInto(window.FixedWindows(15))
         | "Groupby" >> beam.GroupByKey()
         | "Interpolate" >> beam.ParDo(interpolateSensors())
@@ -52,7 +51,7 @@ def run(subscription_name, output_table, output_gcs_path, interval=1.0, pipeline
 
       gcs = (
         data
-        | "convert to csv" >> beam.ParDo(convertToCsv())
+        | "convert to csv" >> beam.Map(convertToCsv)
         | "Write to GCS" >> fileio.WriteToFiles(output_gcs_path)
       )
 
