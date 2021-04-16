9a54bfcab4eae98d9ef0fd6df08ceea2e5835792
Subhabrata1996 <subhabrataiam@gmail.com>
2021-04-17 02:38:53 +0530

    Adding AI Platform files

--- AiPlatformTrainer/trainer/task.py
+++ AiPlatformTrainer/trainer/task.py
@@ -4,10 +4,11 @@ import os
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
-from sklearn.externals import joblib
+from sklearn.metrics import f1_score 
+import joblib
 from google.cloud import storage
 from sklearn import tree
+from datetime import datetime
 import hypertune
 
 def read_from_bigquery(full_table_path, project_id=None, num_samples=None):
@@ -21,12 +22,12 @@ def read_from_bigquery(full_table_path, project_id=None, num_samples=None):
       pandas.DataFrame
     """
     BASE_QUERY = '''
-	    SELECT
-	      *
-	    FROM
-	      `{table}`
-  	'''
-    query = metadata.BASE_QUERY.format(table=full_table_path)
+        SELECT
+          *
+        FROM
+          `{table}`
+      '''
+    query = BASE_QUERY.format(table=full_table_path)
     limit = ' LIMIT {}'.format(num_samples) if num_samples else ''
     query += limit
 
@@ -37,35 +38,34 @@ def read_from_bigquery(full_table_path, project_id=None, num_samples=None):
     return data_df
 
 def label_dataset(dataset):
-	#Our logic to label the data
-	dataset["Status"] = "Healthy"
-	#If all the 5 sensors readings are more than 70 percentile of the distribution, we flag the event at a failure
-	dataset.loc[(dataset["PRESSURE_1"] > np.percentile(dataset["PRESSURE_1"],70))
+    #Our logic to label the data
+    dataset["Status"] = "Healthy"
+    #If all the 5 sensors readings are more than 70 percentile of the distribution, we flag the event at a failure
+    dataset.loc[(dataset["PRESSURE_1"] > np.percentile(dataset["PRESSURE_1"],70))
         & (dataset["PRESSURE_2"] > np.percentile(dataset["PRESSURE_2"],70))
         & (dataset["PRESSURE_3"] > np.percentile(dataset["PRESSURE_3"],70))
         & (dataset["PRESSURE_4"] > np.percentile(dataset["PRESSURE_4"],70))
         & (dataset["PRESSURE_5"] > np.percentile(dataset["PRESSURE_5"],70)),["Status"]] = "Failure"
-    
-    return dataset    
+    return dataset
 
 def train_score_model(dataset):
 
-	#Remove the Timestamp and Status coloumn as they are not out independent variables X
-	X = dataset.drop(["Timestamp","Status"],axis=1)
-	#Declare the dependent variable - we want to predict
-	Y = dataset["Status"]
-	#split the dataset into train and test 80:20
-	X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.20, random_state=10)   
+    #Remove the Timestamp and Status coloumn as they are not out independent variables X
+    X = dataset.drop(["Timestamp","Status"],axis=1)
+    #Declare the dependent variable - we want to predict
+    Y = dataset["Status"]
+    #split the dataset into train and test 80:20
+    X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.20, random_state=10)   
 
-	classifier_DT = tree.DecisionTreeClassifier()
-	classifier_DT = classifier_DT.fit(X_train,Y_train) 
+    classifier_DT = tree.DecisionTreeClassifier()
+    classifier_DT = classifier_DT.fit(X_train,Y_train) 
 
-	Y_pred_DT = classifier_DT.predict(X_test)
-	score = f1_score(Y_test,Y_pred_DT, pos_label="Failure")
-	logging.info('F1 Score: %s', scores)
+    Y_pred_DT = classifier_DT.predict(X_test)
+    score = f1_score(Y_test,Y_pred_DT, pos_label="Failure")
+    logging.info('F1 Score: %s', score)
 
-	#Report the f1 score as a montoring metrics
-	hpt = hypertune.HyperTune()
+    #Report the f1 score as a montoring metrics
+    hpt = hypertune.HyperTune()
     hpt.report_hyperparameter_tuning_metric(
         hyperparameter_metric_tag='Failure Model F1 Score',
         metric_value=scores)
@@ -73,22 +73,22 @@ def train_score_model(dataset):
     return classifier_DT
 
 def save_model_to_gcs(model, path):
-	"""Read data from BigQuery and split into train and validation sets.
+    """Read data from BigQuery and split into train and validation sets.
     Args:
       path: (string) full path of the GCS bucket where out model would be stored
       model: The sklearn model we want to save
     """
-	artifact_filename = 'model.joblib'
+    artifact_filename = 'model.joblib'
 
-	#Dump the model locally
+    #Dump the model locally
     local_path = artifact_filename
     joblib.dump(model, local_path)
 
     model_directory = path
-	storage_path = os.path.join(model_directory, artifact_filename)
-	blob = storage.blob.Blob.from_string(storage_path, client=storage.Client())
-	#Copy the local model.joblib to gcs
-	blob.upload_from_filename(local_path)
+    storage_path = os.path.join(model_directory, artifact_filename)
+    blob = storage.blob.Blob.from_string(storage_path, client=storage.Client())
+    #Copy the local model.joblib to gcs
+    blob.upload_from_filename(local_path)
 
 def _parse_args():
     """Parses command-line arguments."""
@@ -137,30 +137,22 @@ def _parse_args():
 
     return parser.parse_args()
 
+def run_experiment(arguments):
+    logging.info('Arguments: %s', arguments)
+    dataset = read_from_bigquery(arguments.input)   
+    dataset = label_dataset(dataset)    
+    model = train_score_model(dataset)
+    save_model_to_gcs(model, arguments.output_dir)
 
+"""Entry point"""
 def main():
-    """Entry point"""
     arguments = _parse_args()
+    print(arguments)
     logging.basicConfig(level=arguments.log_level)
     # Run the train and evaluate experiment
     time_start = datetime.utcnow()
-    train_model(arguments)
+    run_experiment(arguments)
     time_end = datetime.utcnow()
     time_elapsed = time_end - time_start
     logging.info('Training elapsed time: {} seconds'.format(
         time_elapsed.total_seconds()))
-
-def run_experiment(arguments):
-	logging.info('Arguments: %s', arguments)
-
-	dataset = read_from_bigquery(arguments.input)   
-	
-	dataset = label_dataset(dataset)	
-
-	model = train_score_model(dataset)
-
-	save_model_to_gcs(model, arguments.output_dir)
-
-
-
-
